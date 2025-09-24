f1ef5398ce2b2cb0bf14895edc20f0c0f0957485
fonnymunkey <fonnymunkey@gmail.com>
2025-09-24 19:43:41 -0400

    1.4.6
    Fix SME subject override tweaks

--- src/main/java/rlmixins/RLMixins.java
+++ src/main/java/rlmixins/RLMixins.java
@@ -16,7 +16,7 @@ import rlmixins.proxy.CommonProxy;
 public class RLMixins {
     
     public static final String MODID = "rlmixins";
-    public static final String VERSION = "1.4.5";
+    public static final String VERSION = "1.4.6";
     public static final String NAME = "RLMixins";
     public static final Logger LOGGER = LogManager.getLogger();
 
